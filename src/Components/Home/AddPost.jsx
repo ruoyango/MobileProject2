7e3e66e2a3b750963d8634b3038298d4a52e0b45
1lhc <90663580+1lhc@users.noreply.github.com>
2024-04-03 21:28:58 +0800

    add icons

--- src/Components/Home/AddPost.jsx
+++ src/Components/Home/AddPost.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import '../LoginSignup/LoginSignup.css'
 import logo from '../../logo.svg'
 import home from '../Assets/Home.png'
+import logoutlogo from '../Assets/logoutlogo.png'
+import searchlogo from '../Assets/searchlogo.png'
+import savelogo from '../Assets/savelogo.png'
+import uploadlogo from '../Assets/uploadlogo.png'
 
 const AddPost = () => {
     return (
@@ -9,6 +13,11 @@ const AddPost = () => {
             <div className="header">
                 <img src={logo} className="profile-photo" alt="profile" />
                 <img src={home} className="home" alt="home" />
+                <img src={searchlogo} className="home" alt="search" />
+                <img src={uploadlogo} className="home" alt="upload" />
+                <img src={savelogo} className="home" alt="save" />
+                <img src={logoutlogo} className="home" alt="logout" />
+
                 <div className="submit-container">
                     <div className="submit">Post</div>
                 </div>
