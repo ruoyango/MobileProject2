5ba94139d7281ce11bc775c75a986e744bad9ece
DoDyo <44600980+DoDyo@users.noreply.github.com>
2024-04-07 00:02:14 +0800

    update dashboard

--- src/Components/WebPages/Dashboard.jsx
+++ src/Components/WebPages/Dashboard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import "./style.css"
 import NavBar from "../NavBar"
 import Sidebar from"../NavBar2"
-import test from '../Assets/7ad47d8666404417a572428d9ce78a82.png'
+import test from '../Assets/user.png'
 import * as CiIcons from 'react-icons/ci'
 import { HiOutlineChatBubbleOvalLeft } from "react-icons/hi2"
 import {Link} from "react-router-dom";
